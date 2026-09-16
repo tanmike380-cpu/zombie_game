0a651d2a3a1acda6d34c493e3802e926c8da06a1
tanmike380-cpu <tanmike380@gmail.com>
2026-09-17 02:57:33 +0800

    tech: add one-click Unity benchmark scene generator

---
+++ Assets/_Game/Editor/CreateHordeBenchmarkScene.cs
@@ -0,0 +1,70 @@
+#if UNITY_EDITOR
+using System.IO;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using ZombieGame.Benchmark;
+
+namespace ZombieGame.EditorTools
+{
+    public static class CreateHordeBenchmarkScene
+    {
+        private const string SceneDirectory = "Assets/_Game/Scenes";
+        private const string ScenePath = SceneDirectory + "/Tech_HordeBenchmark.unity";
+
+        [MenuItem("Tools/Zombie Game/Create 10K Horde Benchmark Scene")]
+        public static void CreateScene()
+        {
+            Directory.CreateDirectory(SceneDirectory);
+
+            Scene scene = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Single);
+            scene.name = "Tech_HordeBenchmark";
+
+            GameObject cameraObject = new GameObject("Main Camera");
+            Camera camera = cameraObject.AddComponent<Camera>();
+            cameraObject.tag = "MainCamera";
+            camera.clearFlags = CameraClearFlags.SolidColor;
+            camera.backgroundColor = new Color(0.08f, 0.09f, 0.11f, 1f);
+            camera.fieldOfView = 55f;
+            camera.nearClipPlane = 0.1f;
+            camera.farClipPlane = 500f;
+            cameraObject.transform.position = new Vector3(0f, 105f, -95f);
+            cameraObject.transform.LookAt(Vector3.zero);
+
+            GameObject benchmarkObject = new GameObject("HordeRenderBenchmark");
+            benchmarkObject.AddComponent<HordeRenderBenchmark>();
+
+            GameObject ground = GameObject.CreatePrimitive(PrimitiveType.Plane);
+            ground.name = "Reference Ground";
+            ground.transform.position = Vector3.zero;
+            ground.transform.localScale = new Vector3(14f, 1f, 14f);
+
+            Renderer groundRenderer = ground.GetComponent<Renderer>();
+            Shader shader = Shader.Find("Universal Render Pipeline/Unlit");
+            if (shader == null)
+            {
+                shader = Shader.Find("Unlit/Color");
+            }
+
+            if (shader != null)
+            {
+                Material groundMaterial = new Material(shader)
+                {
+                    name = "BenchmarkGround_RuntimePreview",
+                    color = new Color(0.14f, 0.16f, 0.14f, 1f)
+                };
+                groundRenderer.sharedMaterial = groundMaterial;
+            }
+
+            EditorSceneManager.MarkSceneDirty(scene);
+            EditorSceneManager.SaveScene(scene, ScenePath);
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+
+            Selection.activeGameObject = benchmarkObject;
+            Debug.Log($"Created benchmark scene at {ScenePath}. Open it and press Play.");
+        }
+    }
+}
+#endif
